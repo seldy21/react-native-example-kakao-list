530fac6c2d03bb441647d91641d2afe534848c38
seldy21 <seldy@naver.com>
2024-02-05 19:50:16 +0900

    update

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+const Icon = (props) => {
+  return <AntDesign name={props.name} size={24} color="black" />;
+};
+
+export default () => {
+  return (
+    <View style={{ paddingVertical: 10, paddingHorizontal: 20, flexDirection: "row", justifyContent:"space-between", alignItems:"center"}}>
+      <Text style={{ fontSize: 25, fontWeight: "bold" }}>전체</Text>
+      <View style={{ flexDirection: "row", gap: 6 }}>
+        <Icon name={"search1"} />
+        <Icon name={"adduser"} />
+        <Icon name={"setting"} />
+      </View>
+    </View>
+  );
+};
